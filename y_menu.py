2a448ed8ffed8f9a4869caf56b7cdf3edce58512
Yaroslav Fisyukov <76122377+yarchefis@users.noreply.github.com>
2024-05-25 22:33:00 +0300

    edit

--- y_menu.py
+++ y_menu.py
@@ -4,7 +4,7 @@
 #  > ^ <  
 # ---------------------------------------------------------------------------------
 # Name: MenuButton
-# Description: Модуль, который отправляет сообщение с кнопкой по команде .menu
+# Description: Модуль, который отправляет сообщение с кнопкой по команде .menu и отвечает на личные сообщения с клавиатурой
 # Author: yarchefis
 # Commands:
 # .menu
@@ -18,11 +18,13 @@ logger = logging.getLogger(__name__)
 
 @loader.tds
 class MenuButtonMod(loader.Module):
-    """Модуль, который отправляет сообщение с кнопкой по команде .menu"""
+    """Модуль, который отправляет сообщение с кнопкой по команде .menu и отвечает на личные сообщения с клавиатурой"""
 
     strings = {
         "name": "MenuButton",
         "menu_message": "Нажмите на кнопку ниже:",
+        "choose_action": "Выберите действие:",
+        "hold_message": "Держи!",
     }
 
     async def client_ready(self, client, db):
@@ -41,3 +43,16 @@ class MenuButtonMod(loader.Module):
 
     async def inline__button_pressed(self, call):
         await call.answer("Привет!", show_alert=True)
+
+    async def watcher(self, message: Message):
+        if message.is_private:
+            await self.inline.form(
+                message=message,
+                text=self.strings["choose_action"],
+                reply_markup=[
+                    [{"text": "Конфиг warp", "callback": self.send_hold_message}]
+                ],
+            )
+
+    async def send_hold_message(self, call):
+        await call.answer(self.strings["hold_message"], show_alert=True)
