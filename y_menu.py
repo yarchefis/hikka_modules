da6df7fa185bb3f988496b577d4875c3012c9629
Yaroslav Fisyukov <76122377+yarchefis@users.noreply.github.com>
2024-05-26 11:17:18 +0300

    edit

--- y_menu.py
+++ y_menu.py
@@ -3,11 +3,9 @@
 # ( o.o )  🔓 Not licensed.
 #  > ^ <  
 # ---------------------------------------------------------------------------------
-# Name: MenuButton
-# Description: Модуль, который отправляет сообщение с кнопкой по команде .menu и отвечает на личные сообщения с клавиатурой
+# Name: yMenu
+# Description: Модуль личный помощник.
 # Author: yarchefis
-# Commands:
-# .menu
 # ---------------------------------------------------------------------------------
 
 import logging
@@ -17,42 +15,21 @@ from .. import loader, utils  # type: ignore
 logger = logging.getLogger(__name__)
 
 @loader.tds
-class MenuButtonMod(loader.Module):
-    """Модуль, который отправляет сообщение с кнопкой по команде .menu и отвечает на личные сообщения с клавиатурой"""
+class yMenuMod(loader.Module):
+    """Модуль личный помощник."""
 
     strings = {
-        "name": "MenuButton",
-        "menu_message": "Нажмите на кнопку ниже:",
-        "choose_action": "Выберите действие:",
-        "hold_message": "Держи!",
+        "name": "yMenu",
+        "config_response": "Привет, держи конфиг",
+        "file_url": "https://t.me/c/2244812198/3"
     }
 
     async def client_ready(self, client, db):
         self.client = client
 
-    @loader.unrestricted
-    async def menucmd(self, message: Message):
-        """Отправить сообщение с кнопкой"""
-        await self.inline.form(
-            message=message,
-            text=self.strings["menu_message"],
-            reply_markup=[
-                [{"text": "Нажми меня", "callback": self.inline__button_pressed}]
-            ],
-        )
-
-    async def inline__button_pressed(self, call):
-        await call.answer("Привет!", show_alert=True)
-
     async def watcher(self, message: Message):
         if message.is_private:
-            await self.inline.form(
-                message=message,
-                text=self.strings["choose_action"],
-                reply_markup=[
-                    [{"text": "Конфиг warp", "callback": self.send_hold_message}]
-                ],
-            )
+            if any(word in message.raw_text.lower() for word in ["конфиг", "кфг", "варп", "config", "warp", "kfg"]):
+                await message.reply(self.strings["config_response"])
+                await self.client.send_file(message.chat_id, self.strings["file_url"])
 
-    async def send_hold_message(self, call):
-        await call.answer(self.strings["hold_message"], show_alert=True)
