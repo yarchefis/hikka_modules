0fc4fcfee240e5b00869b549528eec156409f900
Yaroslav Fisyukov <76122377+yarchefis@users.noreply.github.com>
2024-09-23 17:52:21 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,38 +1,24 @@
+import logging
 from telethon.tl.types import Message
-from .. import loader, utils
+from .. import loader, utils  # type: ignore
+
+logger = logging.getLogger(__name__)
 
 @loader.tds
-class MenuMod(loader.Module):
-    """Модуль для отображения меню с кнопками"""
-
+class TestModule(loader.Module):
+    """Модуль с тестовой командой."""
     strings = {
-        "name": "Menu",
-        "menu_message": "📝 <b>Выберите опцию:</b>",
+        "name": "TestModule",
+        "test_message": "тест"
     }
 
+    def __init__(self):
+        pass
+
     async def client_ready(self, client, db):
-        self._me = await client.get_me()
+        self.client = client
 
-    @loader.command
-    async def menu(self, message: Message):
-        """Показать меню"""
-        if message.from_id != self._me.id:
-            await message.reply("У вас нет прав на использование этой команды.")
-            return
-
-        buttons = [
-            [{"text": "Опция 1", "callback": self._handle_option1}],
-            [{"text": "Опция 2", "callback": self._handle_option2}],
-            # Добавьте другие кнопки по мере необходимости
-        ]
-
-        await message.reply(
-            self.strings("menu_message"),
-            reply_markup={"inline_keyboard": buttons},
-        )
-
-    async def _handle_option1(self, call):
-        await call.answer("Вы выбрали опцию 1!")
-
-    async def _handle_option2(self, call):
-        await call.answer("Вы выбрали опцию 2!")
+    @loader.command()
+    async def test(self, message: Message):
+        """Команда для тестирования"""
+        await utils.answer(message, self.strings["test_message"])
