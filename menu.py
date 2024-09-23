ca0dc8cb3cad8bdd8fda77a981c2417453d14b25
Yaroslav Fisyukov <76122377+yarchefis@users.noreply.github.com>
2024-09-23 17:56:04 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 import logging
-from telethon.tl.types import Message, ReplyKeyboardMarkup, KeyboardButton
-from .. import loader, utils  # type: ignore
+from telethon.tl.types import Message
+from .. import loader, utils
 
 logger = logging.getLogger(__name__)
 
@@ -12,16 +12,19 @@ class TestModule(loader.Module):
         "test_message": "тест"
     }
 
-    def __init__(self):
-        pass
-
     async def client_ready(self, client, db):
         self.client = client
 
-    @loader.command()
     async def test(self, message: Message):
         """Команда для тестирования с кнопкой"""
-        button = KeyboardButton("Нажми меня")
-        markup = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
+        await self.inline.form(
+            self.strings["test_message"],
+            message=message,
+            reply_markup={
+                "text": "Нажми меня",
+                "callback": self.handle_button_click
+            }
+        )
 
-        await message.reply(self.strings["test_message"], reply_markup=markup)
+    async def handle_button_click(self, call):
+        await call.answer("Кнопка нажата!")
