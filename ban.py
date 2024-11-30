d8204cf54865f3c1a5dec69ba84f050fef2949df
yardev <76122377+yarchefis@users.noreply.github.com>
2024-11-30 20:45:50 +0300

    Update ban.py

--- ban.py
+++ ban.py
@@ -15,9 +15,10 @@ class SimpleResponderMod(loader.Module):
 
     async def client_ready(self, client, db):
         self.client = client
+        logger.info("SimpleResponderMod загружен и готов к работе.")
 
-    @loader.watcher(incoming=True)
+    @loader.watcher(incoming=True, only_messages=True)
     async def watcher(self, message):
-        if message.is_private and message.sender_id != self.client.me.id:
-            # Отправляем ответ пользователю
+        if message.is_private and message.sender_id != (await self.client.get_me()).id:
+            logger.info(f"Ответ отправлен пользователю: {message.sender_id}")
             await message.reply(self.strings["response_message"])
